5bc62272c56e884d932c305d17e946fd19c4ba4e
whh32 <am.lashkari2017@gmail.com>
2022-04-25 19:38:02 +0430

    Complete custom scrollbar

---
+++ js/script.js
@@ -0,0 +1,21 @@
+let nav = document.querySelector("header");
+let customScroll = document.querySelectorAll(".customScroll > ul > li");
+let parts = document.querySelectorAll(".parts");
+
+window.addEventListener("scroll", checkScroll);
+let scroll = 0;
+function checkScroll() {
+    newScroll = this.scrollY
+    if (newScroll > scroll) {
+        nav.style.top = "-70px"
+    } else {
+        nav.style.top = "0px"
+    }
+    scroll = newScroll
+}
+
+for (let i = 0; i < customScroll.length; i++) {
+    customScroll[i].addEventListener("click", () => {
+        window.scrollTo(0, parts[i].offsetTop)
+    })
+}
