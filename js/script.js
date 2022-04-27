c01963c1e6d3f198c6d78ea7b34bedb43722dd70
whh32 <am.lashkari2017@gmail.com>
2022-04-27 15:56:44 +0430

    fix menu and custom scrollbar

--- js/script.js
+++ js/script.js
@@ -4,22 +4,39 @@ let parts = document.querySelectorAll(".parts");
 
 window.addEventListener("scroll", checkScroll);
 let scroll = 0;
+let newScroll;
 function checkScroll() {
     newScroll = this.scrollY
     if (newScroll > scroll) {
         nav.style.top = "-70px"
-    } else {
+    } else if (newScroll == 0) {
         nav.style.top = "0px"
     }
     scroll = newScroll
 }
 
+
+
 for (let i = 0; i < customScroll.length; i++) {
     customScroll[i].addEventListener("click", () => {
         window.scrollTo(0, parts[i].offsetTop);
-        customScroll.forEach(element => {
-            element.classList.remove("active");
-        });
-        customScroll[i].classList.add("active");
-    })
+    });
+    window.addEventListener("scroll", autoScroll);
+    function autoScroll() {
+        if (newScroll >= (window.innerHeight * i) - window.innerHeight / 2) {
+            customScroll.forEach(element => {
+                element.classList.remove("active");
+            });
+            customScroll[i].classList.add("active");
+            if (i % 2 !== 0) {
+                customScroll.forEach(element => {
+                    element.classList.add("dark")
+                });
+            } else {
+                customScroll.forEach(element => {
+                    element.classList.remove("dark")
+                });
+            }
+        }
+    }
 }
